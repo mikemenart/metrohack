d87d03925cb7127706ed12b46d443717a9baaf78
cayl4434 <cfazio19@jcu.edu>
2018-04-28 18:51:25 -0400

    circle size edits, might be some issues

--- getCircleCords.R
+++ getCircleCords.R
@@ -5,27 +5,28 @@ getCircleCords <- function(viewLat,viewLon,bird){
   degreesOfSeperation<-360/len
   
   #Initialize Dataframe Vectors
+  species <- vector(length = len)
   lat <- vector(length = len)
   long < vector(lenght = len) 
   size <- vector(length = len)
   
   #find lat and lon position of each species
   for (i in 1:len){
+    species[i] <- bird$species[i] ##SOS IS THIS RIGHT??
     originLat<-radiusToMarker*(sin(i*degreesOfSeperation))
     originLon<-radiusToMarker*(cos(i*degreesOfSeperation))
     lat[i]<-originLat+viewLat
     long[i]<-originLon+viewLon
+    
+    #scale the freq into a span
+    radSpanMax<-15
+    radSpanMin<-4
+    m <- (radSpanMax-radSpanMin) / (max(bird$freq))
+    size[i] <- m*(bird$freq[i])+radSpanMin ## NOT SURE IF THIS IS RIGHT!!
+    
     i=i+1
   }
   
-  #scale the freq into 
-  #spanMax<-15
-  #spanMin<-4
-  #m<-
-  #size<-m*bird$freq+b
-  
-  size<-5
   circles <- data.frame(lat, long, size)
   return(circles)
-  
 }
